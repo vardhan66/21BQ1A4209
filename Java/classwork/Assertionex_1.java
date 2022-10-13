aba7a528425004eec0885d284ac7057eae909b21
Eswar <113081338+vardhan66@users.noreply.github.com>
2022-10-13 09:10:49 +0530

    Update Assertionex_1.java

--- Java/classwork/Assertionex_1.java
+++ Java/classwork/Assertionex_1.java
@@ -1,16 +1,18 @@
-
+//To demonstrate Assertion with an example
+//importing Scanner from util package
 import java.util.Scanner;
 
 class AssertionExample {
-    public static void main(String args[]) {
+    public static void main(String args[]) {//main method
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);//creating scanner object
+        //prompt
         System.out.print("Enter ur age ");
-
+        //reading input
         int value = scanner.nextInt();
-        assert value >= 18 : " Not valid";
+        assert value >= 18 : " Not valid";//assertion
 
-        System.out.println("value is " + value);
+        System.out.println("value is " + value);//displaying value
         scanner.close();
-    }
-}
+    }//end of main method
+}//end of class
