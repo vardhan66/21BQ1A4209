d6c7c6d628b9864a586151c6cab94185c73250cd
vardhan66 <21bq1a4209@vvit.net>
2022-11-24 08:50:51 +0530

    Program 13

---
+++ Java/Labcycle/Prog13/EvodG.java
@@ -0,0 +1,62 @@
+import java.lang.Math;
+
+class even implements Runnable {
+    int c;
+
+    even(int x) {
+        c = x;
+    }
+
+    public void run() {
+        try {
+            if (c % 2 == 0) {
+
+                System.out.println("Sqare is :" + c * c);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+    }
+}
+
+class odd implements Runnable {
+    int a;
+
+    odd(int y) {
+        a = y;
+    }
+
+    public void run() {
+        try {
+            if (a % 2 != 0) {
+                System.out.println("Cube is :" + (a * (a * a)));
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        try {
+            int a = (int) (1 + Math.random() * 20);
+            System.out.println("Generated number" + a);
+            if (a % 2 == 0) {
+                Runnable e = new even(a);
+                Thread t = new Thread(e);
+                t.start();
+            } else if (a % 2 != 0) {
+                Runnable o = new odd(a);
+                Thread t1 = new Thread(o);
+                t1.start();
+            }
+            Thread.sleep(1000);
+        } catch (InterruptedException e1) {
+            System.out.println("Interrupted");
+        }
+
+    }
+}
